8dc6a7d1ba38261d3e7907020fbd63dce5e5692f
biswajit <biswajitbannerje@gmail.com>
2025-01-30 13:55:50 -0500

    Comment venv code

--- run.sh
+++ run.sh
@@ -10,9 +10,9 @@ fi
 start_backend() {
     echo "Starting backend server..."
     cd backend
-    python3 -m venv venv
-    source venv/bin/activate
-    pip install -r ../requirements.txt
+    # python3 -m venv venv
+    # source venv/bin/activate
+    # pip install -r ../requirements.txt
     python app.py &
     cd ..
 }
